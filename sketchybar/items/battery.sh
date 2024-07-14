a61c8e1def591d9de7dba81fc028693317140d70
channinghsu <xbc1241888704@gmail.com>
2024-07-14 08:33:27 +0800

    update sketchybar config

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 battery=(
-  script="$PLUGIN_DIR/battery.sh"
-  icon.font="$FONT:Regular:19.0"
-  padding_right=5
-  padding_left=0
-  label.drawing=off
-  update_freq=120
-  updates=on
+	script="$PLUGIN_DIR/battery.sh"
+	icon.font="$FONT:Regular:19.0"
+	icon.padding_right=7
+	padding_right=0
+	padding_left=0
+	label.drawing=on
+	label.padding_right=10
+	update_freq=120
 )
 
-sketchybar --add item battery right      \
-           --set battery "${battery[@]}" \
-           --subscribe battery power_source_change system_woke
+sketchybar --add item battery right \
+	--set battery "${battery[@]}" \
+	--subscribe battery power_source_change system_woke
